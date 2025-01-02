f82428c9efd95b9dcc6fe9f2582cc6de667781d3
khanr <khanriza@gmail.com>
2025-01-02 11:43:13 -0500

    first pass at dap

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,24 @@
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require "dap"
+
+    dap.adapters.php = {
+      type = "executable",
+      command = "node",
+      args = { os.getenv "HOME" .. "/vscode-php-debug/out/phpDebug.js" },
+    }
+
+    dap.configurations.php = {
+      {
+        type = "php",
+        request = "launch",
+        name = "Listen for xdebug",
+        port = "9003",
+        log = true,
+        --  serverSourceRoot = 'localhost:8888',
+        --  localSourceRoot = '~/Sites/',
+      },
+    }
+  end,
+}
