2fce7b578511231c0fbd7000bab91ac59d188812
Riza Khan <khanriza@gmail.com>
2025-01-16 20:11:46 -0500

    adding a better border

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,5 +2,10 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
+  opts = {
+    style = "moon",
+    on_colors = function(colors)
+      colors.border = "#a9b1d6"
+    end,
+  },
 }
