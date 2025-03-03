2bf233811a960e5986c06eb5fbef55bf44a04b60
Riza Khan <riza.khan@appnovation.com>
2025-03-02 19:24:22 -0500

    update

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -6,7 +6,7 @@ return {
     dap.adapters.php = {
       type = "executable",
       command = "node",
-      args = { os.getenv "HOME" .. "/vscode-php-debug/out/phpDebug.js" },
+      args = { os.getenv "HOME" .. "/.vscode/extensions/xdebug.php-debug-1.35.0/out/phpDebug.js" },
     }
 
     dap.configurations.php = {
@@ -16,6 +16,9 @@ return {
         name = "Listen for xdebug",
         port = "9003",
         log = true,
+        pathMappings = {
+          ["/app"] = "${workspaceFolder}", -- Use os.getenv to resolve the home directory
+        },
       },
     }
   end,
