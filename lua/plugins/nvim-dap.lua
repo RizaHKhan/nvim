74349506d5bb54d3e9a429864b01c815a1ba968f
khanr <khanriza@gmail.com>
2025-03-16 17:41:51 -0400

    personal location

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -6,7 +6,7 @@ return {
     dap.adapters.php = {
       type = "executable",
       command = "node",
-      args = { os.getenv "HOME" .. "/.vscode/extensions/xdebug.php-debug-1.35.0/out/phpDebug.js" },
+      args = { os.getenv "HOME" .. ".vscode-server/extensions/xdebug.php-debug-1.35.0/out.phpDebug.js" },
     }
 
     dap.configurations.php = {
