6d60e8a5686def96d2b0817359c09be2b43077b7
Riza Khan <riza.khan@appnovation.com>
2025-03-27 21:06:54 -0400

    going back to original keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -37,7 +37,7 @@ return {
                   end
                 end,
               },
-              [".."] = function()
+              ["<leader>f."] = function()
                 require("telescope.builtin").find_files {
                   initial_mode = "normal",
                   hidden = true,
