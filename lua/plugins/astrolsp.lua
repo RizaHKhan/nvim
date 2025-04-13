a704cbe31497af8a85e9ceb9c2b9af673e017111
RizaHKhan <khanriza@gmail.com>
2025-04-13 11:14:49 -0400

    enabling phpstan

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -51,7 +51,7 @@ return {
           ["language_server.diagnostics_on_update"] = false,
           ["language_server.diagnostics_on_open"] = false,
           ["language_server.diagnostics_on_save"] = false,
-          ["language_server_phpstan.enabled"] = false,
+          ["language_server_phpstan.enabled"] = true,
           ["language_server_psalm.enabled"] = false,
           ["language_server_completion.trim_leading_dollar"] = true,
           ["language_server_php_cs_fixer.enabled true"] = true,
