bcfa63762279a83f559126c22dbac62397af8f2a
Riza Khan <riza.khan@appnovation.com>
2025-05-14 19:46:16 -0400

    using snacks picker and removing telescope

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -11,14 +11,100 @@ return {
     "folke/snacks.nvim",
     opts = {
       bigfile = { enabled = true },
-      explorer = { enabled = false },
+      explorer = { enabled = true },
       indent = { enabled = false },
       input = { enabled = true },
       notifier = {
         enabled = true,
         timeout = 3000,
       },
-      picker = { enabled = true },
+      picker = {
+        transform = function(item)
+          if not item.file then return item end
+          -- Demote the "lazyvim" keymaps file:
+          if item.file:match "lazyvim/lua/config/keymaps%.lua" then item.score_add = (item.score_add or 0) - 30 end
+          -- Boost the "neobean" keymaps file:
+          -- if item.file:match("neobean/lua/config/keymaps%.lua") then
+          --   item.score_add = (item.score_add or 0) + 100
+          -- end
+          return item
+        end,
+        -- In case you want to make sure that the score manipulation above works
+        -- or if you want to check the score of each file
+        debug = {
+          scores = false, -- show scores in the list
+        },
+        -- I like the "ivy" layout, so I set it as the default globaly, you can
+        -- still override it in different keymaps
+        layout = {
+          preset = "ivy",
+          -- When reaching the bottom of the results in the picker, I don't want
+          -- it to cycle and go back to the top
+          cycle = false,
+        },
+        layouts = {
+          -- I wanted to modify the ivy layout height and preview pane width,
+          -- this is the only way I was able to do it
+          -- NOTE: I don't think this is the right way as I'm declaring all the
+          -- other values below, if you know a better way, let me know
+          --
+          -- Then call this layout in the keymaps above
+          -- got example from here
+          -- https://github.com/folke/snacks.nvim/discussions/468
+          ivy = {
+            layout = {
+              box = "vertical",
+              backdrop = false,
+              row = -1,
+              width = 0,
+              height = 0.5,
+              border = "top",
+              title = " {title} {live} {flags}",
+              title_pos = "left",
+              { win = "input", height = 1, border = "bottom" },
+              {
+                box = "horizontal",
+                { win = "list", border = "none" },
+                { win = "preview", title = "{preview}", width = 0.5, border = "left" },
+              },
+            },
+          },
+          -- I wanted to modify the layout width
+          --
+          vertical = {
+            layout = {
+              backdrop = false,
+              width = 0.8,
+              min_width = 80,
+              height = 0.8,
+              min_height = 30,
+              box = "vertical",
+              border = "rounded",
+              title = "{title} {live} {flags}",
+              title_pos = "center",
+              { win = "input", height = 1, border = "bottom" },
+              { win = "list", border = "none" },
+              { win = "preview", title = "{preview}", height = 0.4, border = "top" },
+            },
+          },
+          matcher = {
+            frecency = true,
+          },
+          win = {
+            input = {
+              keys = {
+                ["<Esc>"] = { "close", mode = { "n", "i" } },
+              },
+            },
+          },
+          formatters = {
+            file = {
+              filename_first = true, -- display filename before the file path
+              truncate = 80,
+            },
+          },
+        },
+      },
       quickfile = { enabled = true },
       scope = { enabled = true },
       scroll = { enabled = false },
@@ -48,7 +134,56 @@ return {
       },
     },
     keys = {
-      { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+      {
+        "<leader><space>",
+        function()
+          Snacks.picker.files {
+            finder = "files",
+            format = "file",
+            show_empty = true,
+            supports_live = true,
+            -- In case you want to override the layout for this keymap
+            -- layout = "vscode",
+          }
+        end,
+        desc = "Smart Find Files",
+      },
+      { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+      {
+        "H",
+        function()
+          Snacks.picker.buffers {
+            -- I always want my buffers picker to start in normal mode
+            on_show = function() vim.cmd.stopinsert() end,
+            finder = "buffers",
+            format = "buffer",
+            hidden = false,
+            unloaded = true,
+            current = true,
+            sort_lastused = true,
+            win = {
+              input = {
+                keys = {
+                  ["d"] = "bufdelete",
+                },
+              },
+              list = { keys = { ["d"] = "bufdelete" } },
+            },
+            -- In case you want to override the layout for this keymap
+            -- layout = "ivy",
+          }
+        end,
+        desc = "Buffers",
+      },
+      {
+        "K",
+        function()
+          Snacks.picker.git_diff {
+            on_show = function() vim.cmd.stopinsert() end,
+          }
+        end,
+        desc = "Find Git Diff Files",
+      },
       { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
       { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
       { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
@@ -83,6 +218,8 @@ return {
       },
     },
   },
+
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 
   -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = false },
