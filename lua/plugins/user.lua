028e6570efaa35622441cdf83d423c71d9f973b9
RizaHKhan <khanriza@gmail.com>
2025-08-03 09:00:10 -0400

    removing gruvbox

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -378,19 +378,6 @@ return {
     end,
   },
   {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_enable_italic = true
-      vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_float_style = "dim"
-    end,
-  },
-  {
     "gioele/vim-autoswap",
   },
   {
