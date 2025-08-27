1509d2df8ad9e4d7e02dc7015c8d4ad01d172c2d
RizaHKhan <khanriza@gmail.com>
2025-08-27 13:40:07 -0400

    adding extension to mcp

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -424,7 +424,18 @@ return {
             "nvim-lua/plenary.nvim",
         },
         build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
-        config = function() require("mcphub").setup() end,
+        config = function()
+            require("mcphub").setup {
+                extensions = {
+                    copilotchat = {
+                        enabled = true,
+                        convert_tools_to_functions = true, -- Convert MCP tools to CopilotChat functions
+                        convert_resources_to_functions = true, -- Convert MCP resources to CopilotChat functions
+                        add_mcp_prefix = false, -- Add "mcp_" prefix to function names
+                    },
+                },
+            }
+        end,
     },
     {
         "mistweaverco/kulala.nvim",
