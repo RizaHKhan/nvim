c87efe9d088afa4383cd3f5860fe76f5ee30e88d
Riza Khan <khanriza@gmail.com>
2025-10-13 10:30:01 -0400

    moving codecompanion to its own page

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -358,39 +358,6 @@ return {
         end,
     },
     {
-        "olimorris/codecompanion.nvim",
-        config = true,
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
-            "j-hui/fidget.nvim", -- Display status
-            "ravitemer/codecompanion-history.nvim", -- Save and load conversation history
-        },
-        opts = {
-            extensions = {
-                mcphub = {
-                    callback = "mcphub.extensions.codecompanion",
-                    opts = {
-                        show_result_in_chat = true, -- Show mcp tool results in chat
-                        make_vars = true, -- Convert resources to #variables
-                        make_slash_commands = true, -- Add prompts as /slash commands
-                    },
-                },
-            },
-            strategies = {
-                chat = {
-                    adapter = "copilot",
-                },
-                inline = {
-                    adapter = "copilot",
-                },
-            },
-            opts = {
-                log_level = "DEBUG",
-            },
-        },
-    },
-    {
         "kndndrj/nvim-dbee",
         dependencies = {
             "MunifTanjim/nui.nvim",
