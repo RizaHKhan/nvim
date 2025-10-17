9a4c0538b8d14ade855855b42d1ac4353e9391ce
Riza Khan <khanriza@gmail.com>
2025-10-17 13:05:33 -0400

    adding biome for formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -17,6 +17,7 @@ return {
             -- Set a formatter
             -- null_ls.builtins.formatting.stylua,
             null_ls.builtins.formatting.prettier,
+            null_ls.builtins.formatting.biome,
         })
     end,
 }
