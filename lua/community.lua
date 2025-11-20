21da92b8d1fdd79dc34baa2ad70bd23dc52e7e29
Riza Khan <khanriza@gmail.com>
2025-11-19 21:15:03 -0500

    adding laravel

--- lua/community.lua
+++ lua/community.lua
@@ -25,6 +25,7 @@ return {
     { import = "astrocommunity.pack.html-css" },
     { import = "astrocommunity.pack.lua" },
     { import = "astrocommunity.pack.nginx" },
+    { import = "astrocommunity.pack.laravel" },
     { import = "astrocommunity.pack.php" },
     { import = "astrocommunity.pack.python" },
     { import = "astrocommunity.pack.sql" },
