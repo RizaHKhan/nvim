fe7593d7ea70e378af8ef961e31913266ddd52d9
Riza Khan <khanriza@gmail.com>
2026-01-29 09:07:55 -0500

    adding fzf-lua

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -222,4 +222,16 @@ return {
         priority = 1000,
         opts = {},
     },
+    {
+        "ibhagwan/fzf-lua",
+        -- optional for icon support
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        -- or if using mini.icons/mini.nvim
+        -- dependencies = { "nvim-mini/mini.icons" },
+        ---@module "fzf-lua"
+        ---@type fzf-lua.Config|{}
+        ---@diagnostic disable: missing-fields
+        opts = {},
+        ---@diagnostic enable: missing-fields
+    },
 }
