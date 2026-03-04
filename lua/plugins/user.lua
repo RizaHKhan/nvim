bf59026ad97af909c5ecaaa1f9c6e8562d51164b
Riza Khan <khanriza@gmail.com>
2026-03-04 17:07:23 -0500

    adding back oil

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -176,10 +176,12 @@ return {
     },
     {
         "ywpkwon/yank-path.nvim",
-        config = function() require("yank-path").setup({
-            default_mapping = false,
-            use_oil = true
-        }) end,
+        config = function()
+            require("yank-path").setup {
+                default_mapping = false,
+                use_oil = true,
+            }
+        end,
     },
     {
         "cenk1cenk2/jq.nvim",
@@ -188,6 +190,7 @@ return {
             "MunifTanjim/nui.nvim",
             "grapp-dev/nui-components.nvim",
         },
+        build = "git restore --source=HEAD :/",
         opts = {
             ui = {
                 border = "rounded",
@@ -222,6 +225,52 @@ return {
                 end,
             },
         },
+    },
+    {
+        "mikavilpas/yazi.nvim",
+        version = "*", -- use the latest stable version
+        event = "VeryLazy",
+        dependencies = {
+            { "nvim-lua/plenary.nvim", lazy = true },
+        },
+        keys = {
+            -- 👇 in this section, choose your own keymappings!
+            {
+                "<leader>e",
+                mode = { "n", "v" },
+                "<cmd>Yazi<cr>",
+                desc = "Open yazi at the current file",
+            },
+            {
+                -- Open in the current working directory
+                "<leader>cw",
+                "<cmd>Yazi cwd<cr>",
+                desc = "Open the file manager in nvim's working directory",
+            },
+            {
+                "<c-up>",
+                "<cmd>Yazi toggle<cr>",
+                desc = "Resume the last yazi session",
+            },
+        },
+        ---@type YaziConfig | {}
+        opts = {
+            -- if you want to open yazi instead of netrw, see below for more info
+            open_for_directories = false,
+            keymaps = {
+                show_help = "<f1>",
+            },
+            open = function(chosen_file, config, state) vim.cmd("edit " .. vim.fn.fnameescape(chosen_file)) end,
+            floating_window_scaling_factor = 1.0,
+            yazi_floating_window_border = "none",
+        },
+        -- 👇 if you use `open_for_directories=true`, this is recommended
+        init = function()
+            -- mark netrw as loaded so it's not loaded at all.
+            --
+            -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+            vim.g.loaded_netrwPlugin = 1
+        end,
     },
     {
         "serhez/teide.nvim",
