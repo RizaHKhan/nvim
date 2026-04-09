b0657fed985f6233f9defd252191a780fda40248
Riza Khan <khanriza@gmail.com>
2026-04-08 23:29:05 -0400

    python

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,14 +1,15 @@
 return {
     "nvim-neotest/neotest",
     dependencies = {
+        "nvim-neotest/neotest-python",
         "olimorris/neotest-phpunit",
         "nvim-neotest/neotest-jest",
         "marilari88/neotest-vitest",
         "V13Axel/neotest-pest",
     },
-    opts = {
+    opts = function(_, opts)
         -- See all config options with :h neotest.Config
-        discovery = {
+        opts.discovery = {
             -- Drastically improve performance in ginormous projects by
             -- only AST-parsing the currently opened buffer.
             enabled = false,
@@ -16,31 +17,27 @@ return {
             -- A value of 0 automatically assigns number based on CPU.
             -- Set to 1 if experiencing lag.
             concurrent = 1,
-        },
-        running = {
+        }
+        opts.running = {
             -- Run tests concurrently when an adapter provides multiple commands to run.
             concurrent = true,
-        },
-        summary = {
+        }
+        opts.summary = {
             -- Enable/disable animation of icons.
             animated = false,
-        },
-    },
-    config = function()
-        require("neotest").setup {
-            adapters = {
-                require "neotest-python",
-                require "neotest-vitest",
-                require "neotest-phpunit" {
-                    root_ignore_files = { "tests/Pest.php" },
-                },
-                require "neotest-pest" {},
-                require "neotest-jest" {
-                    jestCommand = "npm test --",
-                    jestConfigFile = "custom.jest.config.ts",
-                    env = { CI = true },
-                    cwd = function(path) return vim.fn.getcwd() end,
-                },
+        }
+        opts.adapters = {
+            require "neotest-python",
+            require "neotest-vitest",
+            require "neotest-phpunit" {
+                root_ignore_files = { "tests/Pest.php" },
+            },
+            require "neotest-pest" {},
+            require "neotest-jest" {
+                jestCommand = "npm test --",
+                jestConfigFile = "custom.jest.config.ts",
+                env = { CI = true },
+                cwd = function(_) return vim.fn.getcwd() end,
             },
         }
     end,
